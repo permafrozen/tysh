bf453ac592bae0c565f8bd7ff4684584468a8d0e
Permafrozen <m8tt8o@gmail.com>
2025-05-13 12:40:36 +0200

    added flake environment + direnv

---
+++ dev.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# inspired by @ricecracker2234 on dc
+
+reload() {
+    pkill gjs
+    ags run ./src/app.ts &
+}
+
+ags quit
+reload
+
+while inotifywait -e close_write -r ./src; do
+    reload
+done
---
+++ flake.nix
@@ -0,0 +1,70 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    ags = {
+      url = "github:aylur/ags?ref=v3";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ flake-parts, ags, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+        let
+          pname = "tysh";
+          entry = "./src/app.ts";
+          version = "0.0.1";
+
+          astalPackages = with ags.packages.${system}; [
+            io
+            astal4
+            battery
+            network
+            hyprland
+            notifd
+          ];
+
+          extraPackages = astalPackages ++ [ pkgs.libadwaita pkgs.libsoup_3 ];
+
+        in {
+          packages.default = pkgs.stdenv.mkDerivation {
+            src = ./.;
+            inherit pname;
+            inherit version;
+
+            # build dependencies
+            nativeBuildInputs = with pkgs; [
+              wrapGAppsHook
+              gobject-introspection
+              ags.packages.${system}.default
+            ];
+
+            # runtime dependencies
+            buildInputs = extraPackages ++ [ pkgs.gjs ];
+
+            installPhase = ''
+              runHook preInstall
+
+              mkdir -p $out/bin
+              mkdir -p $out/share
+              cp -r * $out/share
+              ags bundle ${entry} $out/bin/${pname} -d "SRC='$out/share'"
+
+              runHook postInstall
+            '';
+
+          };
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              (ags.packages.${system}.default.override {
+                inherit extraPackages;
+              })
+            ];
+          };
+        };
+    };
+}
